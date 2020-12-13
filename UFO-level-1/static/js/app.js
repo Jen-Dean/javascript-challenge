23c765e06bb4ac683be5baba1ad958199bf8498d
Jen-Dean <missjenniferdean@gmail.com>
2020-12-13 11:01:26 -0500

    updates

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,27 @@
-// from data.js
-var tableData = data;
+/////////////////// Creating the Base Table
 
-// YOUR CODE HERE!
+var tableData = data;
+var tableElement = d3.select("#ufo-table")
+var tbody = d3.select("tbody")
+
+
+data.forEach((item) => {
+    var row = tbody.append("tr");
+    Object.entries(item).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+/////////////////// Creating the Clickable form
+
+var button = d3.select("#filter-btn");
+var form = d3.select(".form-control");
+
+
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+function runEnter() {
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datetime");
